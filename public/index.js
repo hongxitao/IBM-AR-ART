2840ca5325fc132278641c6532b20746447bade7
Sally Piao <piaosally@gmail.com>
2022-07-17 16:40:06 +0800

    weather request successfully

--- public/index.js
+++ public/index.js
@@ -82,29 +82,30 @@ AFRAME.registerComponent('school-playground', {
             });
         }
 
-// function getWeather(){
-//   const request = require('request');
-
-//   var latitude = 51.502108;
-//   var longitude = -0.116684;
-
-//   const options = {
-//     method: 'GET',
-//     url: 'https://api.weather.com/v1/geocode/' + latitude + '/' + longitude + '/observations.json',
-//     qs: {
-//         units: 'e',
-//         language: 'en-GB',
-//         apiKey: '29244201b19a4508a44201b19ad508c1'
-//     },
-//     headers: {accept: 'application/json'}
-//   };
-
-//   request(options, function (error, response, body) {
-//     if (error) throw new Error(error);
-
-//     jsondata = JSON.parse(body)
-//     console.log(jsondata)
-//     console.log("current condition:", jsondata["observation"]["wx_phrase"])
-//   });
+        // function getWeather() {
+        //     const request = require('request');
+        //
+        //     var latitude = 51.502108;
+        //     var longitude = -0.116684;
+        //
+        //     const options = {
+        //         method: 'GET',
+        //         url: 'https://api.weather.com/v1/geocode/' + latitude + '/' + longitude + '/observations.json',
+        //         qs: {
+        //             units: 'e',
+        //             language: 'en-GB',
+        //             apiKey: '29244201b19a4508a44201b19ad508c1'
+        //         },
+        //         headers: {accept: 'application/json'}
+        //     };
+        //
+        //     request(options, function (error, response, body) {
+        //         if (error) throw new Error(error);
+        //
+        //         jsondata = JSON.parse(body)
+        //         console.log(jsondata)
+        //         console.log("current condition:", jsondata["observation"]["wx_phrase"])
+        //     });
+        // }
     }
 })
