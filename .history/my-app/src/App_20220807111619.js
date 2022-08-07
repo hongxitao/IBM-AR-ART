ef406235fd304e134161b70096a2563d09afe461
Sally Piao <46841790+1752124@users.noreply.github.com>
2022-08-07 11:21:29 +0800

    react framework

---
+++ .history/my-app/src/App_20220807111619.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+      <a href="boxtest.html"><button> box test </button></a>
+      <a href="weather.html"><button>  weathertest </button></a>
+      <a href="LocationBasedAR.html"><button>  locationtest </button></a>
+      <a href="LocationBasedAR copy.html"><button>  locationtest1 </button></a>
+      <a href="LocationBasedAR copy2.html"><button>  locationtest2 </button></a>
+      <a href="LocationBasedAR copy3.html"><button>  locationtest3 </button></a>
+    </div>
+  );
+}
+
+export default App;
